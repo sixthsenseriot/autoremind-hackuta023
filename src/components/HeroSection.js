468d71cc8727e1de73bd7783673767e86e5037e0
Khanh <knh.nguyen2002@gmail.com>
2023-10-07 17:18:52 -0500

    Made HeroSection background into car video

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,7 +6,7 @@ import "./HeroSection.css";
 function HeroSection() {
   return (
     <div className="hero-container">
-      <video src="/videos/video-1.mp4" autoPlay loop muted />
+      <video src="/videos/car-1.mp4" autoPlay loop muted />
       <h1>NEVER MISS A CAR INSPECTION</h1>
       <p>Treat your vehicle right!</p>
       <div className="hero-btns">
