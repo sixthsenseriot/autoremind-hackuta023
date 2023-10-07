1be0024d96370ed0545df4b517b0ebaf51a59e25
Khanh <knh.nguyen2002@gmail.com>
2023-10-07 14:04:44 -0500

    Coded buttons for navbar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
 
 function App() {
   return (
     <>
       <Router>
         <Navbar />
-        <Switch></Switch>
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
       </Router>
     </>
   );
