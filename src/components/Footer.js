c27eacd0c187ccce9f735375fdd547de8e3dd9bd
allierodrigues <albamaria20013@gmail.com>
2023-10-08 09:39:37 -0500

    edited footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -39,18 +39,11 @@ function Footer() {
             <h2>Contact Us</h2>
             <Link to="/">Contact</Link>
             <Link to="/">Support</Link>
-            <Link to="/">Destinations</Link>
             <Link to="/">Sponsorships</Link>
           </div>
         </div>
         <div className="footer-link-wrapper">
-          <div class="footer-link-items">
-            <h2>Videos</h2>
-            <Link to="/">Submit Video</Link>
-            <Link to="/">Ambassadors</Link>
-            <Link to="/">Agency</Link>
-            <Link to="/">Influencer</Link>
-          </div>
+          
           <div class="footer-link-items">
             <h2>Social Media</h2>
             <Link to="/">Instagram</Link>
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,7 +7,7 @@ function HeroSection() {
   return (
     <div className="hero-container">
       <video src="/videos/car-1.mp4" autoPlay loop muted />
-      <h1>NEVER FORGET CAR MAINTENANCE</h1>
+      <h1>NEVER FORGET<br></br> CAR MAINTENANCE</h1>
       <p>Treat your vehicle right!</p>
       <div className="hero-btns">
         <Button
